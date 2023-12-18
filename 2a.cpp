27de1908a441f6522ea0687ab54367eec2cc8355
adrianarmm <areyemor@myuax.com>
2023-12-18 08:59:16 +0100

    23

--- 2a.cpp
+++ 2a.cpp
@@ -4,3 +4,7 @@
 const char* compararCadenas(const char* cadena1, const char* cadena2) {
     int comparacion = strcmp(cadena1, cadena2);
     if (comparacion == 0) {
+        return "Las cadenas son iguales.";
+    } else {
+        return "Las cadenas son diferentes.";
+    }
