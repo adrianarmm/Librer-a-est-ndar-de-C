52551cc4e781e6cba10a5b3b193396d597db2fdc
adrianarmm <areyemor@myuax.com>
2023-12-18 09:09:48 +0100

    57

--- 3b.cpp
+++ 3b.cpp
@@ -14,5 +14,7 @@ int main() {
         printf("El archivo no se pudo abrir para lectura.\n");
         return 1;
     }
-
+    char buffer[50];
+    if (fgets(buffer, sizeof(buffer), file) != NULL) {
+        printf("Contenido del archivo: %s\n", buffer);
 }
