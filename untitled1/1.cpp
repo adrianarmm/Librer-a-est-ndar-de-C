b5c8c4c19f888bd5da88248dfa62a2c41d3d1665
adrianarmm <areyemor@myuax.com>
2023-12-18 08:57:35 +0100

    18

--- untitled1/1.cpp
+++ untitled1/1.cpp
@@ -10,20 +10,3 @@ int main() {
     return 0;
 }
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-
-int main() {
-    srand(time(0));
-
-    int limite_inferior = 0;
-    int limite_superior = 100;
-
-    int rango = limite_superior - limite_inferior + 1;
-    int random_number = rand() % rango + limite_inferior;
-
-    printf("Número aleatorio entre %d y %d: %d\n", limite_inferior, limite_superior, random_number);
-
-    return 0;
-}
