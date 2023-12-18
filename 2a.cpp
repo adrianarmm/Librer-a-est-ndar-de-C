72bb6b3c0ee2c0e85e71b4c4dfaece9e6c79b632
adrianarmm <areyemor@myuax.com>
2023-12-18 08:59:07 +0100

    22

--- 2a.cpp
+++ 2a.cpp
@@ -1,2 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+
+const char* compararCadenas(const char* cadena1, const char* cadena2) {
+    int comparacion = strcmp(cadena1, cadena2);
+    if (comparacion == 0) {
