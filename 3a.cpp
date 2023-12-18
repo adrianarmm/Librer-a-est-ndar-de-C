274bf1f73ae9159c2f037ab02bdb1e9b15b5fae2
adrianarmm <areyemor@myuax.com>
2023-12-18 09:06:57 +0100

    48

--- 3a.cpp
+++ 3a.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    FILE *file = fopen("3a", "r");
+    FILE *file = fopen("2a", "r");
     if (file == NULL) {
         printf("No se pudo abrir el archivo.\n");
         return 1;
